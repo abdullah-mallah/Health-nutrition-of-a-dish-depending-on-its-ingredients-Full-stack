cfa37898b00e1a5c94a2984543e49b1a4cc80cbd
omar zarifa <omarzarifa@gmail.com>
2024-04-30 17:05:45 +0200

    Edamam api

---
+++ routes/recipeRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/:food", async (req, res) => {
+    const food = req.params.food;
+    const url = `https://api.edamam.com/api/recipes/v2?q=${food}&app_id=4f21898d&app_key=4c3e681eb31124f74461092eb4196d07&type=public`;
+    try {
+        const response = await axios.get(url);
+        // Extract both label and calories from each recipe
+        const recipes = response.data.hits.map(hit => {
+            return {
+                label: hit.recipe.label,
+                calories: Math.round(hit.recipe.calories) // Round the calories to a whole number
+            };
+        });
+        res.json(recipes); // Send labels and calories as JSON
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+        res.status(500).send('Failed to fetch recipes');
+    }
+});
+
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const usersRoutes = require('./routes/userRoutes'); // Import user routes
+const recipeRoutes = require('./routes/recipeRoutes');
 require("dotenv").config(); //import .env file
 const express = require("express");
 const connectDB = require('./connection.js'); // Import the MongoDB connection
@@ -11,7 +12,10 @@ connectDB(); // Connect to MongoDB
 const path = require("path");//
 app.use(express.static(path.join(__dirname, "public", )));
 app.use(bodyParser.json());//to use bodyparse in express
+
+// routes
 app.use('/api/users', usersRoutes); // Attach user routes under '/api/users'
+app.use('/api/recipes', recipeRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
