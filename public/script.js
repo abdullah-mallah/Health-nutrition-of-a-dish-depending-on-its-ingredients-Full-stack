90c88976f220e01e0478768748fd13f899c6dc60
Mustafa.Qassmieh01 <mustafqasmieh@gmail.com>
2024-05-18 05:19:57 +0200

    made the home.html still there is the images not appearing for some reason

--- public/script.js
+++ public/script.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
             displayWelcomeMessage(userName);
         }
 
-    home()
+    // home()
   } else if (path.includes('ingredients')) {
     fetchIngrediants();
   } else if (path.includes('favourites')) {
@@ -803,34 +803,34 @@ function getFavouriteRecipes() {
 
 
 //////////// home tab functions
-function home() { 
-  // Get all images
-  var images = document.querySelectorAll('.home_food-image');
-  var currentIndex = 0;
+// function home() { 
+//   // Get all images
+//   var images = document.querySelectorAll('.home_food-image');
+//   var currentIndex = 0;
 
-  function showImage(index) {
-    // Hide all images
-    images.forEach(function (image) {
-      image.style.display = 'none';
-    });
-    // Show the current image
-    images[index].style.display = 'block';
-  }
+//   function showImage(index) {
+//     // Hide all images
+//     images.forEach(function (image) {
+//       image.style.display = 'none';
+//     });
+//     // Show the current image
+//     images[index].style.display = 'block';
+//   }
 
-  function nextImage() {
-    currentIndex++;
-    if (currentIndex >= images.length) {
-      currentIndex = 0;
-    }
-    showImage(currentIndex);
-  }
+//   function nextImage() {
+//     currentIndex++;
+//     if (currentIndex >= images.length) {
+//       currentIndex = 0;
+//     }
+//     showImage(currentIndex);
+//   }
 
-  // Show the first image initially
-  showImage(currentIndex);
+//   // Show the first image initially
+//   showImage(currentIndex);
 
-  // Change image every 3 seconds
-  setInterval(nextImage, 3000);
-}
+//   // Change image every 3 seconds
+//   setInterval(nextImage, 3000);
+// }
 
 function fetchArticlesFromAPI(numberOfArticles) {
 }
